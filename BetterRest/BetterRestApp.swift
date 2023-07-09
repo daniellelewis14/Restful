e481bce21f71689faf832b655b83739be3d4d452
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-08 22:27:55 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Danielle Lewis on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
